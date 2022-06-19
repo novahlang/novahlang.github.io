beb0fdd80dea33ff1a1ec0da9172f11fe927277a
Bianca Zanardi <bianca.zanardi@gmail.com>
2022-06-19 22:10:57 +0200

    add mobile navigation  the nav uses the unit dvh, not yet supported on Chrome  improve focus management

---
+++ public/js/main.js
@@ -0,0 +1,59 @@
+function closeNav() {
+  const links = document.querySelector('.nav-list');
+  const nav = document.querySelector('nav');
+  const button = document.querySelector('button');
+
+  nav.classList.add('closed');
+  nav.classList.remove('open');
+  links.classList.add('visually-hidden');
+  button.setAttribute('aria-expanded', false);
+}
+
+function openNav() {
+  const links = document.querySelector('.nav-list');
+  const nav = document.querySelector('nav');
+  const button = document.querySelector('button');
+
+  nav.classList.add('open');
+  nav.classList.remove('closed');
+  links.classList.remove('visually-hidden');
+  button.setAttribute('aria-expanded', true);
+}
+
+function onNavToggle() {
+  const links = document.querySelector('.nav-list');
+  const nav = document.querySelector('nav');
+
+  if (nav.classList.contains('open')) {
+    closeNav();
+  } else {
+    openNav();
+    links.firstElementChild.firstElementChild.focus();
+  }
+}
+
+function forceCloseNav() {
+  if (window.innerWidth < 1030) {
+    closeNav();
+  } else {
+    openNav();
+  }
+}
+
+function escapeNav(e) {
+  if (e.keyCode === 27) {
+    closeNav();
+    const main = document.querySelector('#main');
+    main.firstElementChild.focus();
+  }
+}
+
+let debounceTimer;
+const debounce = (callback, time) => {
+  window.clearTimeout(debounceTimer);
+  debounceTimer = window.setTimeout(callback, time);
+};
+
+window.onload = forceCloseNav;
+window.onresize = () => debounce(forceCloseNav, 100);
+window.addEventListener('keyup', e => escapeNav(e));
