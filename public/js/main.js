21c8868fe87dfdf0c80e0603e1cf6f39527d512a
Bianca Zanardi <bianca.zanardi@gmail.com>
2022-06-20 21:59:06 +0200

    improve keyboard navigation  change hover color

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
+const MIN_WIDTH = 1030;
+
 function closeNav() {
-  const links = document.querySelector('.nav-list');
   const nav = document.querySelector('nav');
+  const links = document.querySelector('.nav-list');
   const button = document.querySelector('button');
 
   nav.classList.add('closed');
@@ -10,8 +12,8 @@ function closeNav() {
 }
 
 function openNav() {
-  const links = document.querySelector('.nav-list');
   const nav = document.querySelector('nav');
+  const links = document.querySelector('.nav-list');
   const button = document.querySelector('button');
 
   nav.classList.add('open');
@@ -21,30 +23,75 @@ function openNav() {
 }
 
 function onNavToggle() {
-  const links = document.querySelector('.nav-list');
-  const nav = document.querySelector('nav');
+    const nav = document.querySelector('nav');
 
   if (nav.classList.contains('open')) {
     closeNav();
   } else {
     openNav();
-    links.firstElementChild.firstElementChild.focus();
+    const firstLi = document.querySelector('.nav-list').firstElementChild;
+    firstLi.focus();
   }
 }
 
 function forceCloseNav() {
-  if (window.innerWidth < 1030) {
+  if (window.innerWidth < MIN_WIDTH) {
     closeNav();
   } else {
     openNav();
   }
 }
 
-function escapeNav(e) {
-  if (e.keyCode === 27) {
+function handleKeyEvents(e) {
+  const focused = document.activeElement;
+  const nav = document.querySelector('nav');
+
+  // escape on nav in mobile view
+  if (e.keyCode === 27 && nav.classList.contains('open') && window.innerWidth < MIN_WIDTH) {
     closeNav();
-    const main = document.querySelector('#main');
-    main.firstElementChild.focus();
+    const button = document.querySelector('button');
+    button.focus();
+  }
+
+  // arrow down on burger button
+  else if (e.keyCode === 40 && focused?.getAttribute('aria-controls') === 'nav') {
+    openNav();
+    const firstLink = document.querySelector('.nav-list').firstElementChild.firstElementChild;
+    firstLink.focus();
+  }
+
+  // arrow down on nav
+  else if (e.keyCode === 40 && focused?.parentElement?.getAttribute('role') === 'menuitem') {
+    const nextLink = focused?.parentElement?.nextElementSibling?.firstElementChild;
+    if (nextLink) nextLink.focus();
+    else {
+      const firstLink = document.querySelector('.nav-list').firstElementChild.firstElementChild;
+      firstLink.focus();
+    }
+  }
+
+  // arrow up on burger button
+  else if (e.keyCode === 38 && focused?.getAttribute('aria-controls') === 'nav') {
+    openNav();
+    const lastLink = document.querySelector('.nav-list').lastElementChild.firstElementChild;
+    lastLink.focus();
+  }
+
+  // arrow up on nav
+  else if (e.keyCode === 38 && focused?.parentElement?.getAttribute('role') === 'menuitem') {
+    const prevLink = focused?.parentElement?.previousElementSibling?.firstElementChild;
+    if (prevLink) prevLink.focus();
+    else {
+      const lastLink = document.querySelector('.nav-list').lastElementChild.firstElementChild;
+      lastLink.focus();
+    }
+  }
+}
+
+function handleLostFocus(e) {
+  // focus went outside nav in mobile view
+  if (e.relatedTarget?.parentElement?.parentElement?.className !== 'nav-list' && window.innerWidth < MIN_WIDTH) {
+    forceCloseNav();
   }
 }
 
@@ -56,8 +103,8 @@ const debounce = (callback, time) => {
 
 window.onload = forceCloseNav;
 window.onresize = () => debounce(forceCloseNav, 100);
-window.addEventListener('keyup', e => escapeNav(e));
+window.addEventListener('keyup', e => handleKeyEvents(e));
 window.addEventListener('DOMContentLoaded', () => {
   const lastLink = document.querySelector('.nav-list').lastElementChild.firstElementChild;
-  lastLink.addEventListener('blur', forceCloseNav);
+  lastLink.addEventListener('focusout', handleLostFocus);
 });
